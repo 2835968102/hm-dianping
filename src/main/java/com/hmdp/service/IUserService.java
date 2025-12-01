2a32a5f402a082d8641950f9c210d539b0a6d2e8
pml <2835968102@qq.com>
2025-12-01 17:18:39 +0800

    添加用户登录校验

--- src/main/java/com/hmdp/service/IUserService.java
+++ src/main/java/com/hmdp/service/IUserService.java
@@ -1,6 +1,7 @@
 package com.hmdp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 
@@ -17,6 +18,9 @@ import javax.servlet.http.HttpSession;
  */
 public interface IUserService extends IService<User> {
 
+    // 发送验证码
     Result sendCode (String phone, HttpSession session);
 
+    // 登录校验
+    Result login(LoginFormDTO loginForm, HttpSession session);
 }
--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
@@ -43,6 +44,46 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 
         // 6.返回ok
         return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session){
+        // 1.校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            // 2.如果不符合，返回错误信息
+            return Result.fail("手机号格式错误！");
+        }
+        // 3.校验验证码
+        Object cacheCode = session.getAttribute("code");
+        String code = loginForm.getCode();
+        if(cacheCode == null || !cacheCode.toString().equals(code)){
+            //3.不一致，报错
+            return Result.fail("验证码错误");
+        }
+        //一致，根据手机号查询用户
+        User user = query().eq("phone", phone).one();
+
+        //5.判断用户是否存在
+        if(user == null){
+            //不存在，则创建
+            user =  createUserWithPhone(phone);
+        }
+        //7.保存用户信息到session中
+        session.setAttribute("user",user);
+
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        // 新建一个user
+        User user = new User();
+        user.setPhone(phone);
+
+        // 保存到数据库中
+        save(user);
+
+        return user;
     }
 
 }
